6f66ca7922cd5c3dba1fe45ef650242ec0b0b1f6
Antiloope <rodrigopizarro1234@gmail.com>
2018-05-29 16:14:43 -0300

    Listo agregar personas

--- api/controllers/PersonaController.js
+++ api/controllers/PersonaController.js
@@ -6,20 +6,46 @@
  */
 
 module.exports = {
-  agregar: async function (req, res){
+  agregar: async function (req, res) {
+    var param = req.allParams();
+    var nuevapersona = {
+      nombre: param.nombre,
+      apellido: param.apellido,
+      dni: param.dni,
+      correo: param.correo,
+      direccion: param.direccion,
+    };
+
+    datos = await Persona.create(nuevapersona);
+    var personas = await Persona.find({});
+    res.view('pages/personas/listasa', {persona: personas});
+  },
+  modificar: async function (req, res) {
 
   },
-  modificar: async function (req, res){
+  eliminar: async function (req, res) {
+
+    console.log(JSON.stringify(req.allParams()));
+    res.redirect('/lista');
+  },
+  devolverFormulario: async function (req, res) {
+    if (req == null){
+      var ret;
+      ret = {
+        nombre: null,
+        apellido: null,
+        dni: null,
+        correo: null,
+        direccion: null
+      };
+    } else ret = req;
+    res.view('pages/personas/formulario', {persona: ret});
+  },
+  buscar: async function (req, res) {
 
   },
-  eliminar: async function (req, res){
-
-  },
-  buscar: async function (req, res){
-
-  },
-  lista: async function (req, res){
-    var personas = await Usuario.find({});
+  lista: async function (req, res) {
+    var personas = await Persona.find({});
 
     res.view('pages/personas/listasa', {persona: personas});
   },
