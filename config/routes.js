f1cf7c2fe3680a01b5923411c63f3de5364a2a67
hernanavarro <navarro.hernan97@gmail.com>
2018-05-30 10:54:47 -0300

    El que anda

--- config/routes.js
+++ config/routes.js
@@ -129,7 +129,7 @@ module.exports.routes = {
         Asistencia (Fortnite)
    */
   'GET /asistencia':{
-    controller: 'asistenciaController',
+    controller: 'AsistenciaController',
     action: 'lista',
   }
 
