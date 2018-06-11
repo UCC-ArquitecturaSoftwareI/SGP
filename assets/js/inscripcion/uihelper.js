be03540323bfc8ac87f719aad9f32da94af8d78d
Gaspar de Elias <caspercba@gmail.com>
2018-06-10 23:39:21 -0300

    Fixed some semicolons

--- assets/js/inscripcion/uihelper.js
+++ assets/js/inscripcion/uihelper.js
@@ -5,9 +5,7 @@
 $('#removeStudentModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget);
   var alumno = button.data('alumno-nombre');
-  var inscripcionId = button.data('inscripcion-id')
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.alumno').text('Alumno: ' + alumno)
-})
+  var inscripcionId = button.data('inscripcion-id');
+  var modal = $(this);
+  modal.find('.alumno').text('Alumno: ' + alumno);
+});
