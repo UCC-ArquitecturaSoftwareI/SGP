a0ea73b1555dfe8653f2929ac2870a83dbb32550
Gaspar de Elias <caspercba@gmail.com>
2018-07-17 00:40:30 -0300

    Agregamos metodo de modelo para obtener facilmente los inscriptos a un curso

--- api/models/Inscripcion.js
+++ api/models/Inscripcion.js
@@ -23,8 +23,30 @@ module.exports = {
       type: 'boolean',
       defaultsTo: false,
     },
-
   },
 
+  // Metodos estaticos  ---
+
+
+  /**
+   * Devuelve un array de personas inscriptas a un curso
+   * @param opts Mapa que contiene cursoId
+   * @returns {Promise<*>}
+   *
+   * Ejemplo:
+   *     var inscriptos = await Inscripcion.getInscriptos({cursoId: 1});
+   */
+
+  getInscriptos: async function(opts) {
+
+    // Buscamos inscripciones al cursoId y llenamos datos de persona
+    var inscripciones = await Inscripcion.find({curso: opts.cursoId}).populate('persona');
+
+    // Descartamos las inscripciones dadas de baja y tambien las personas borradas
+    inscripciones = inscripciones.filter(inscripcion => inscripcion.baja === false && inscripcion.persona.borrada === false);
+
+    // Devolvemos la lista de personas inscriptas
+    return await inscripciones.map(inscripcion => inscripcion.persona);
+  },
 };
 
